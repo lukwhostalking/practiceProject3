1d7d1f89b54f12c4153b711efdac40628d3aa30a
ANUPRIYA AGRAWAL <anupriyaagrawal@ANUPRIYAs-MacBook-Air.local>
2014-06-25 16:51:26 -0400

    Initial Commit

---
+++ practiceProject3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  practiceProject3
+//
+//  Created by ANUPRIYA AGRAWAL on 6/25/14.
+//  Copyright (c) 2014 IshanBhalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ practiceProject3Tests/practiceProject3Tests.m
@@ -0,0 +1,34 @@
+//
+//  practiceProject3Tests.m
+//  practiceProject3Tests
+//
+//  Created by ANUPRIYA AGRAWAL on 6/25/14.
+//  Copyright (c) 2014 IshanBhalla. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface practiceProject3Tests : XCTestCase
+
+@end
+
+@implementation practiceProject3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
